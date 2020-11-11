b6744f222cd52208bb27916a0e12775470bc5579
Adelbert Chang <adelbertc@gmail.com>
2020-11-10 20:26:11 -0800

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 use futures::stream::{self, StreamExt};
+use owning_ref::{OwningHandle, StableAddress};
 use std::ops::Deref;
-use tracing::{info, info_span, Span};
 use tracing::span::Entered;
-use owning_ref::OwningRef;
-use owning_ref::OwningHandle;
-use owning_ref::StableAddress;
-
-struct TracedElement<'a, A> {
-    a: A,
-    span: OwningHandle<SpanWrapper, Box<Entered<'a>>>,
-}
+use tracing::{info, info_span, Span};
 
 struct SpanWrapper(Box<Span>);
 
 impl SpanWrapper {
     fn new(span: Span) -> SpanWrapper {
         SpanWrapper(Box::new(span))
+    }
+
+    fn span(&self) -> Span {
+        (*(*self).0).clone()
     }
 }
 
@@ -26,21 +23,29 @@ impl Deref for SpanWrapper {
     }
 }
 
-unsafe impl StableAddress for SpanWrapper { }
+unsafe impl StableAddress for SpanWrapper {}
 
-impl<'a, A: Clone> TracedElement<'a, A> {
+struct TracedElement<'a, A> {
+    a: A,
+    span: OwningHandle<SpanWrapper, Box<Entered<'a>>>,
+}
+
+impl<'a, A> TracedElement<'a, A> {
     fn new(a: A, span: Span) -> TracedElement<'a, A> {
         let span = OwningHandle::new_with_fn(SpanWrapper::new(span), |span| {
             let entered = unsafe { span.as_ref().unwrap().enter() };
             Box::new(entered)
         });
 
-        TracedElement {
-            a,
-            span: span,
-        }
+        TracedElement { a, span: span }
     }
 
+    fn span(&self) -> Span {
+        self.span.as_owner().span()
+    }
+}
+
+impl<'a, A: Clone> TracedElement<'a, A> {
     fn value(&self) -> A {
         self.a.clone()
     }
@@ -54,12 +59,11 @@ impl<'a, A> Deref for TracedElement<'a, A> {
     }
 }
 
-
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let v: Vec<i32> = vec![1,2,3,4,5,6,7,8,9,10];
+    let v: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     let stream = stream::iter(v)
         .map(|i| TracedElement::new(i, info_span!("my span", value = i)))
